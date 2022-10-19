a808c7a0227620d70a53b22c46b8eda4a13cf68a
seosum <ssm347574248@gmail.com>
2022-10-19 16:27:03 +0900

    jdbc 설정

--- src/main/java/org/zerock/controller/lecture/p04attribute/Controller17.java
+++ src/main/java/org/zerock/controller/lecture/p04attribute/Controller17.java
@@ -3,6 +3,8 @@ package org.zerock.controller.lecture.p04attribute;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.domain.lecture.JavaBean03;
+import org.zerock.domain.lecture.Student;
 
 @Controller
 @RequestMapping("ex17")
@@ -21,8 +23,45 @@ public class Controller17 {
 		model.addAttribute("email", "abc@daum.net");
 	}
 	
+	@RequestMapping("sub03")
+	public void method3(Model model) {
+		model.addAttribute("name", "son hm");
+		model.addAttribute("address", "london");
+		model.addAttribute("age", 99);
+		model.addAttribute("email", "hm@daum.net");
+	}
 	
+	@RequestMapping("sub04")
+	public void method4(Model model) {
+		JavaBean03 obj = new JavaBean03();
+		obj.setAvg(9.99);
+		obj.setClassName("spring");
+		obj.setLocation("seoul");
+		obj.setScore(100);
+		obj.setStudentNumber("s80");
+		
+		model.addAttribute("student", obj);
+	}
+	
+	@RequestMapping("sub05")
+	public void method5(Model model) {
+		Student s = new Student();
+		s.setName("park ji sung");
+		s.setClassName("soccer");
+		s.setStudentNumber("13");
+		
+//		model.addAttribute("student", s);
+		model.addAttribute(s);
+		model.addAttribute("hello");
+	}
 }
+
+
+
+
+
+
+
 
 
 
