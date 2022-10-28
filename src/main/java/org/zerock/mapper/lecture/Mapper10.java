79ac8101f43d98a80dd99b1aef7bbc3b49a3dcd0
seosum <ssm347574248@gmail.com>
2022-10-28 16:43:44 +0900

    20221028

--- src/main/java/org/zerock/mapper/lecture/Mapper10.java
+++ src/main/java/org/zerock/mapper/lecture/Mapper10.java
@@ -1,13 +1,14 @@
 package org.zerock.mapper.lecture;
 
 import org.zerock.domain.lecture.JavaBean18;
+import org.zerock.domain.lecture.JavaBean19;
 
 public interface Mapper10 {
 	
 	int removeCustomer();
 
 	int removeEmployee();
-
+	
 	int removeCustomerById(int id);
 
 	int removeEmployeeById(int id);
@@ -15,6 +16,25 @@ public interface Mapper10 {
 	int addCustomer();
 
 	int addSupplier();
-
+	
 	int insertCustomer(JavaBean18 customer);
+
+	int insertSupplier(JavaBean19 supplier);
+
+	int insertCustomerAndGetKey(JavaBean18 customer);
+
+	int insertSupplierAndKey(JavaBean19 supplier);
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
